f4364edd498d38d5b9831acb08c38c97c77d5bfd
joeyenfield <joey.enfield@gmail.com>
2016-04-25 16:21:48 +0100

    Initial Project Setup
    Check out basic project

---
+++ android/src/com/emptypockets/nodewar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.emptypockets.nodewar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.emptypockets.nodewar.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
